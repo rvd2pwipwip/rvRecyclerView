ab3e9f3fba115d4db91568d8b7a4d8ef1262a85c
Herve Desrosiers <hdesrosiers@stingray.com>
2020-07-29 13:17:58 -0400

    Basic RecyclerView - Just RecyclerView, Adapter and ViewHolder

--- app/src/main/java/com/hdesrosiers/rvrecyclerview/ExampleAdapter.kt
+++ app/src/main/java/com/hdesrosiers/rvrecyclerview/ExampleAdapter.kt
@@ -1,15 +1,41 @@
 package com.hdesrosiers.rvrecyclerview
 
+import android.graphics.Color
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.example_item.view.*
 
-class ExampleAdapter: RecyclerView.Adapter() {
+class ExampleAdapter(private  val exampleList: List<ExampleItem>): RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder>() {
 
+    //each ViewHolder object keeps 3 references to the 3 views inside the item layout
     class ExampleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val imageView = itemView.findViewById<ImageView>(R.id.item_image_view)
-        val titleView = itemView.findViewById<TextView>(R.id.item_title_text_view)
-        val descriptionView = itemView.findViewById<TextView>(R.id.item_description_text_view)
+        val imageView: ImageView = itemView.item_image_view //synthetic property: same as below but more concise
+        val titleView: TextView = itemView.findViewById<TextView>(R.id.item_title_text_view)
+        val descriptionView: TextView = itemView.findViewById<TextView>(R.id.item_description_text_view)
     }
+
+    //only called a few times (enough to fill the container plus a few extras above and below)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExampleViewHolder {
+        val itemView: View = LayoutInflater.from(parent.context).inflate(R.layout.example_item, parent, false)
+        return ExampleViewHolder(itemView) //returns inflated itemView layout
+    }
+
+    //called every time new content needs to be displayed in a (recycled) ViewHolder
+    override fun onBindViewHolder(holder: ExampleViewHolder, position: Int) {
+        val currentItem = exampleList[position] //which item from data set
+        holder.imageView.setImageResource(currentItem.imageResource)
+        holder.titleView.text = currentItem.itemTitleText
+        holder.descriptionView.text = currentItem.itemDescriptionText
+
+//        if (position == 0) {
+//            holder.titleView.setBackgroundColor(Color.DKGRAY)
+//        }
+    }
+
+    override fun getItemCount() = exampleList.size
+
 }
--- app/src/main/java/com/hdesrosiers/rvrecyclerview/MainActivity.kt
+++ app/src/main/java/com/hdesrosiers/rvrecyclerview/MainActivity.kt
@@ -2,10 +2,35 @@ package com.hdesrosiers.rvrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val exampleList = generateDummyList(50)
+        recycler_view.adapter = ExampleAdapter(exampleList)
+        recycler_view.layoutManager = LinearLayoutManager(this)
+        recycler_view.setHasFixedSize(true)
+    }
+
+    private fun generateDummyList(size: Int): List<ExampleItem> {
+        val list = ArrayList<ExampleItem>()
+
+        for (i in 0 until size) {
+            val drawable = when (i % 3) {
+                0 -> R.drawable.the_beatles_sgt_peppers
+                1 -> R.drawable.gorillaz_plastic_beach
+                else -> R.drawable.u2_war
+            }
+
+            val item = ExampleItem(drawable, "Album Title", "Artist Name")
+            list += item
+        }
+
+        return list
     }
 }
