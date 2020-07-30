cd65cc1a45b5e4f095255d681c9f3c55aadf3447
Herve Desrosiers <hdesrosiers@stingray.com>
2020-07-30 07:35:42 -0400

    Insert and remove with notifyItemInsert/Remove

--- app/src/main/java/com/hdesrosiers/rvrecyclerview/ExampleAdapter.kt
+++ app/src/main/java/com/hdesrosiers/rvrecyclerview/ExampleAdapter.kt
@@ -14,6 +14,7 @@ class ExampleAdapter(private  val exampleList: List<ExampleItem>): RecyclerView.
     //each ViewHolder object keeps 3 references to the 3 views inside the item layout
     class ExampleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imageView: ImageView = itemView.item_image_view //synthetic property: same as below but more concise
+        val numberView: TextView = itemView.item_number_text
         val titleView: TextView = itemView.findViewById<TextView>(R.id.item_title_text_view)
         val descriptionView: TextView = itemView.findViewById<TextView>(R.id.item_description_text_view)
     }
@@ -28,6 +29,7 @@ class ExampleAdapter(private  val exampleList: List<ExampleItem>): RecyclerView.
     override fun onBindViewHolder(holder: ExampleViewHolder, position: Int) {
         val currentItem = exampleList[position] //which item from data set
         holder.imageView.setImageResource(currentItem.imageResource)
+        holder.numberView.text = "${position + 1}"
         holder.titleView.text = currentItem.itemTitleText
         holder.descriptionView.text = currentItem.itemDescriptionText
 
--- app/src/main/java/com/hdesrosiers/rvrecyclerview/MainActivity.kt
+++ app/src/main/java/com/hdesrosiers/rvrecyclerview/MainActivity.kt
@@ -2,22 +2,27 @@ package com.hdesrosiers.rvrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private val exampleList = generateDummyList(50)
+    private val adapter = ExampleAdapter(exampleList)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val exampleList = generateDummyList(50)
-        recycler_view.adapter = ExampleAdapter(exampleList)
+        recycler_view.adapter = adapter
         recycler_view.layoutManager = LinearLayoutManager(this)
         recycler_view.setHasFixedSize(true)
     }
 
-    private fun generateDummyList(size: Int): List<ExampleItem> {
+    private fun generateDummyList(size: Int): ArrayList<ExampleItem> {
         val list = ArrayList<ExampleItem>()
 
         for (i in 0 until size) {
@@ -32,5 +37,20 @@ class MainActivity : AppCompatActivity() {
         }
 
         return list
+    }
+
+    fun insertItem(view: View) {
+        val index = Random.nextInt(8)
+        val newItem = ExampleItem(R.drawable.u2_war, "Inserted at position $index", "Artist Name")
+        exampleList.add(index, newItem)
+//        adapter.notifyDataSetChanged() //old way, resets whole list, no animation
+        adapter.notifyItemInserted(index) //might put in adapter for better encapsulation
+    }
+
+    fun removeItem(view: View) {
+        val index = Random.nextInt(8)
+        exampleList.removeAt(index)
+//        adapter.notifyDataSetChanged() //old way, resets whole list, no animation
+        adapter.notifyItemRemoved(index)
     }
 }
